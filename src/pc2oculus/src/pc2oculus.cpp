fd387730ceef97dce71c46dd3164c4d0baf794db
Deakin-mamort <mamort@deakin.edu.au>
2016-01-02 11:53:40 +1100

    Improved rendering

--- src/pc2oculus/src/pc2oculus.cpp
+++ src/pc2oculus/src/pc2oculus.cpp
@@ -3,38 +3,58 @@
 #include <pcl_ros/point_cloud.h>
 #include <std_msgs/String.h>
 #include <geometry_msgs/Point.h>
+#include <sensor_msgs/PointCloud.h>
 #include <pcl/point_types.h>
 #include <pcl_ros/filters/filter.h>
 #include <boost/foreach.hpp>
 #include <sstream>
 
 typedef pcl::PointCloud<pcl::PointXYZ> PointCloud;
-int count;
-void callback(const PointCloud::ConstPtr& unfiltered)
+//  ros::NodeHandle nh;
+sensor_msgs::PointCloud cloud;
+
+
+
+class pubsub
 {
+public:
+    pubsub();
+    void callback(const PointCloud::ConstPtr& image);
+private:
+    ros::NodeHandle nh;
+    ros::Subscriber sub;
+    ros::Publisher pub;
+};
 
-  ros::NodeHandle nh;
-  ros::Publisher pub = nh.advertise<geometry_msgs::Point> ("/points", 100);
-  geometry_msgs::Point p;
+pubsub::pubsub() {
+    pub = nh.advertise<sensor_msgs::PointCloud>("/points", 1000);
+    sub = nh.subscribe<PointCloud>("/camera/depth/points", 1000, &pubsub::callback, this);
+}
 
-  pcl::PointCloud<pcl::PointXYZ>::Ptr filtered(new pcl::PointCloud<pcl::PointXYZ>);
-  std::vector<int> indices;
-  pcl::removeNaNFromPointCloud(*unfiltered,*filtered, indices);
+void pubsub::callback(const PointCloud::ConstPtr& unfiltered)
+{
+      geometry_msgs::Point32 p;
 
-  BOOST_FOREACH (const pcl::PointXYZ& pt, filtered->points){
-//        std::cout << pt.x << ", " << pt.y << ", " << pt.z << "\n";
-        p.x = pt.x;
-        p.y = pt.y;
-        p.z = pt.z;
-        pub.publish(p);
+      pcl::PointCloud<pcl::PointXYZ>::Ptr filtered(new pcl::PointCloud<pcl::PointXYZ>);
+      std::vector<int> indices;
+      pcl::removeNaNFromPointCloud(*unfiltered,*filtered, indices);
 
-  }
+      BOOST_FOREACH (const pcl::PointXYZ& pt, filtered->points){
+    //        std::cout << pt.x << ", " << pt.y << ", " << pt.z << "\n";
+            p.x = pt.x;
+            p.y = pt.y;
+            p.z = pt.z;
+            cloud.points.push_back(p);
+      }
+
+     pub.publish(cloud);
+     cloud.points.clear();
+
 }
 
 int main(int argc, char** argv)
 {
   ros::init(argc, argv, "pc2oculus");
-  ros::NodeHandle nh;
-  ros::Subscriber sub = nh.subscribe<PointCloud>("/camera/depth/points", 1, callback);
+  pubsub ps;
   ros::spin();
 }
